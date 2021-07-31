7a5abee48b0c6fa8fba4a99493b39afb0effea07
Deluxan Mariathasan <deluxan_m@outlook.com>
2021-07-31 19:45:48 +0530

    routes

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const connectDB = require("./config/db");
 
+// Routes
+const usersRoutes = require("./routes/users.routes");
+const productsRoutes = require("./routes/products.routes");
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+
+app.use("/api/users", usersRoutes);
+app.use("/api/products", productsRoutes);
 
 app.get("/", (req, res) => res.send("Hi"));
 
